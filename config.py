cabe12fba0e13bd29141ee17f1bf20ef89c40851
yash <sauravk0257@gmail.com>
2026-08-11 12:07:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # Git
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Vhal999Vhal999/QUEENNEWMUSIC")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
